7ea7c1e9dc35abedaf1a04708472e330ba514b51
ENRIQUE GUILLERMO VILCHEZ LIZANO <enrique.vilchezlizano@ucr.ac.cr>
2023-09-23 15:27:42 -0600

    Modelos Categoria y CategoriaPertenece con algunos atributos, aun pendientes.

---
+++ source/LoCoMPro/Models/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+namespace LoCoMPro.Models
+{
+    [PrimaryKey(nameof(nombre))]
+    public class Categoria
+    {
+        // Nombre
+        [StringLength(256, MinimumLength = 1)]
+        [RegularExpression(@"")]
+        [Display(Name = "Categoría")]
+        public required string nombre { get; set; }
+
+        // Colecciones
+        // public ICollection<Producto>? productos { get; set; }
+
+        // TODO(Los pinwino): Arreglar
+        /*
+        public ICollection<CategoriaPertenece>? categoriasPadre { get; set; }
+
+        public ICollection<CategoriaPertenece>? categoriasHijo { get; set; }
+        */
+    }
+}
