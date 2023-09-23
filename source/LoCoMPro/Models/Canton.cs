c16be77673930b0325f0a5cfc96d81f85996488f
Angie Solis <angie.solismanzano@ucr.ac.cr>
2023-09-23 12:13:33 -0600

    Modelos de Provincia y Cantón con sus atributos y llaves respectivas

---
+++ source/LoCoMPro/Models/Canton.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoCoMPro.Models
+{
+    [PrimaryKey(nameof(nombre), nameof(nombreProvincia))]
+    public class Canton
+    {
+        // Nombre
+        [StringLength(20, MinimumLength = 1)]
+        [RegularExpression(@"")]
+        [Display(Name = "Nombre del cantón")]
+        public string nombre { get; set; }
+
+        // Nombre provincia
+        [StringLength(10, MinimumLength = 1)]
+        [RegularExpression(@"")]
+        [Display(Name = "Nombre de la provincia")]
+        public string nombreProvincia { get; set; }
+
+
+        [ForeignKey("nombreProvincia")]
+        public Provincia provincia { get; set; }
+    }
+}
---
+++ source/LoCoMPro/Models/Provincia.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoCoMPro.Models
+{
+    [PrimaryKey(nameof(nombre))]
+    public class Provincia
+    {
+        // Nombre
+        [StringLength(10, MinimumLength = 1)]
+        [RegularExpression(@"")]
+        [Display(Name = "Nombre de la provincia")]
+        public string nombre { get; set; }
+    }
+}
