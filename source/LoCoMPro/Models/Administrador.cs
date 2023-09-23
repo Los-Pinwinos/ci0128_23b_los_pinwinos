463d74a45750f9502786009576dcc1d99c0a7d5f
kdaniel1652 <kennethvillalobos16@gmail.com>
2023-09-23 14:39:26 -0600

    Modelos AdminstraSobre y ModeraSobre incormpletos

--- source/LoCoMPro/Models/Administrador.cs
+++ source/LoCoMPro/Models/Administrador.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LoCoMPro.Models
 {
