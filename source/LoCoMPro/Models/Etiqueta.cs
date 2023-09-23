97b204d0424672807f8646f0eebff0735472e911
Luis <luisdi2509@gmail.com>
2023-09-23 17:20:51 -0600

    Modelo Etiqueta implementado.

---
+++ source/LoCoMPro/Models/Etiqueta.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LoCoMPro.Models
+{
+    [PrimaryKey(nameof(etiqueta), nameof(creacion), nameof(usuarioCreador))]
+    public class Etiqueta
+    {
+        // Etiqueta
+        [StringLength(256, MinimumLength = 1)]
+        [RegularExpression(@"")]
+        [Display(Name = "Etiquetas")]
+        public required string etiqueta { get; set; }
+
+        // TODO(Los Pinwinos): Investigar DisplayFormatAttribute annotation
+        // Fecha y hora de creación
+        [DataType(DataType.DateTime)]
+        [Range(typeof(DateTime), "9/9/2023", "9/9/2040")]
+        [Display(Name = "Fecha y hora de creación")]
+        public required DateTime creacion { get; set; }
+
+        // Usuario creador
+        [StringLength(12, MinimumLength = 10)]
+        [Display(Name = "Crreador")]
+        public required string usuarioCreador { get; set; }
+
+        // Propiedad de navegación registro
+        // TODO(Emilia): Integrar con Registro
+        /*
+        [ForeignKey("creacion, usuarioCreador")]
+        public Registro registro { get; set; } = null!;
+        */
+    }
+}
