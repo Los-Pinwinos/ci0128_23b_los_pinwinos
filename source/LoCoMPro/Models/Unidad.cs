a719e062d43bde50659d1fad292ffed7ef27484d
ENRIQUE GUILLERMO VILCHEZ LIZANO <enrique.vilchezlizano@ucr.ac.cr>
2023-09-23 16:33:41 -0600

    Modelo de Unidad.

---
+++ source/LoCoMPro/Models/Unidad.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoCoMPro.Models
+{
+    [PrimaryKey(nameof(nombre))]
+    public class Unidad
+    {
+        // Nombre
+        [StringLength(20, MinimumLength = 1)]
+        [RegularExpression(@"")]
+        [Display(Name = "Unidad de medida")]
+        public required string nombre { get; set; }
+
+        // Collecciones
+        // public ICollection<Producto>? productos { get; set; }
+    }
+}
