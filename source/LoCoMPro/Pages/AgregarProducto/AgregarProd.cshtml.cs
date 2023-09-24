5cf006a3ba1ca6268e610ab1a90399f39f3f8a6f
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2023-09-24 11:54:58 -0600

    Botón de agregar y cancelar llaman a funciones iniciales

--- source/LoCoMPro/Pages/AgregarProducto/AgregarProd.cshtml.cs
+++ source/LoCoMPro/Pages/AgregarProducto/AgregarProd.cshtml.cs
@@ -8,5 +8,18 @@ namespace LoCoMPro.Pages.AgregarProducto
         public void OnGet()
         {
         }
+
+        public void OnPostAceptar()
+        {
+            // Lógica para manejar la acción cuando se presiona el botón "Aceptar"
+            Console.WriteLine("Boton de aceptar fue presionado");
+
+        }
+
+        public void OnPostCancelar()
+        {
+            // Lógica para manejar la acción cuando se presiona el botón "Cancelar
+             Console.WriteLine("Boton de cancelar fue presionado");
+        }
     }
 }
