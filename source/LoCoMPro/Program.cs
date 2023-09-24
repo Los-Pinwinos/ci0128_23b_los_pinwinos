49d9aab65a0967f13d0b0d457a855ec49f65ea36
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2023-09-23 19:35:29 -0600

    Actualización del archivo Program.cs

--- source/LoCoMPro/Program.cs
+++ source/LoCoMPro/Program.cs
@@ -1,9 +1,15 @@
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.Extensions.DependencyInjection;
+using LoCoMPro.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<LoCoMProContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LoCoMProContext") ?? throw new InvalidOperationException("Connection string 'LoCoMProContext' not found.")));
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AddPageRoute("/Home/Index", "");  // Set default Index route to home
+});
 
 var app = builder.Build();
 
@@ -13,6 +19,22 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else  // This is for errors while migration data
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+
+// This creates the data base if it is not created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<LoCoMProContext>();
+    context.Database.EnsureCreated();
+    // This feeds the DB if there is nothing
+    // DbInitializer.Initialize(context);
 }
 
 app.UseHttpsRedirection();
