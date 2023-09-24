203a9ccae8b1066800bb9eecfa1f63ad11c634b0
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2023-09-23 20:05:38 -0600

    Se creó "Agregar Producto" en el menú superior de la pantalla y este lleva a una página en blanco.

---
+++ source/LoCoMPro/Pages/AgregarProducto/AgregarProd.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages.AgregarProducto
+{
+    public class AgregarProdModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
