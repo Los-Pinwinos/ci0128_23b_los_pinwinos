6afebdb4c49ad51fde6575d0c884e33437f2d240
Luis <luisdi2509@gmail.com>
2023-09-25 19:53:13 -0600

    Estructura inicial de AgregarRegistro

---
+++ source/LoCoMPro/Pages/AgregarRegistro/AgregarRegistro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages.AgregarRegistro
+{
+    public class AgregarRegistro : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
