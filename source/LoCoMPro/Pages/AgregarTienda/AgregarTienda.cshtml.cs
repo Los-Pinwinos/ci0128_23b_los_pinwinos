963aded03ae22af8cb0460b7f97e7818b4c23d3b
Angie Solis <angie.solismanzano@ucr.ac.cr>
2023-09-25 21:10:06 -0600

    Creación de Agregar Tienda y su respectivo espacio en el menú.

---
+++ source/LoCoMPro/Pages/AgregarTienda/AgregarTienda.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages.AgregarTienda
+{
+    public class AgregarTiendaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
