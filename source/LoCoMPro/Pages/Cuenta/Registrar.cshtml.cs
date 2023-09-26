adc3c4f31e887dedfd55caf33c89053008e352b2
kdaniel1652 <kennethvillalobos16@gmail.com>
2023-09-25 22:51:47 -0600

    Se creó la ventana para registrar usuarios con su respectivo HTML y CSS

---
+++ source/LoCoMPro/Pages/Cuenta/Registrar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages.Cuenta
+{
+    public class ModeloRegistrar : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
