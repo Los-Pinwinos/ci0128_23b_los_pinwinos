9d3435e3914e32424f68084b41959de8d3eff2a5
ENRIQUE GUILLERMO VILCHEZ LIZANO <enrique.vilchezlizano@ucr.ac.cr>
2023-09-28 20:53:13 -0600

    Agregado modelo visual de índice de búsqueda y método GET.

--- source/LoCoMPro/Pages/Busqueda/Index.cshtml.cs
+++ source/LoCoMPro/Pages/Busqueda/Index.cshtml.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using LoCoMPro.Models;
+using LoCoMPro.ViewModels.Busqueda;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 
 namespace LoCoMPro.Pages.Busqueda
@@ -25,10 +27,80 @@ namespace LoCoMPro.Pages.Busqueda
         // Busquedas
         [BindProperty(SupportsGet = true)]
         public string? producto { get; set; }
-       
-        public void OnGet()
+
+        // Visual
+        [BindProperty]
+        public IList<IndexVM> productosVM { get; set; } = default!;
+
+        // On Get Async
+        public async Task OnGetAsync(string? nombreProducto)
         {
-            
+            if (_context.Productos != null)
+            {
+                // Crear el arreglo de productos
+                productosVM = new List<IndexVM>();
+
+                // Asignar
+                if (!string.IsNullOrEmpty(nombreProducto))
+                {
+                    producto = nombreProducto;
+                }
+
+                // Hacer la consulta
+                IQueryable<Producto> productosIQ = (from s in _context.Productos
+                                                    select s).Include(p => p.registros); ;
+                // Ver si se usa el nombre de busqueda
+                productosIQ = buscarProductos(productosIQ);
+
+                // Mappee a la vista
+                await listarProductos(productosIQ);
+            }
+        }
+
+        // Buscar productos
+        public IQueryable<Producto> buscarProductos(IQueryable<Producto> productosIQ)
+        {
+            // Ver si se usa el nombre de busqueda
+            if (!string.IsNullOrEmpty(producto))
+            {
+                return productosIQ.Where(s => s.nombre.Contains(producto));
+            }
+            else
+            {
+                return productosIQ;
+            }
+        }
+
+        // Listar productos
+        public async Task listarProductos(IQueryable<Producto> productosIQ)
+        {
+            // Obtener resultados
+            IList<Producto> productos = await productosIQ.ToListAsync();
+
+            // Por cada resultado
+            foreach (var producto in productos)
+            {
+                // Verificar que el producto tenga registros asociados
+                if (producto.registros != null)
+                {
+                    // Obtener el registro mas reciente
+                    Registro registroMasReciente = producto.registros
+                        .OrderByDescending(r => r.creacion)
+                        .ToList().First();
+                    // Agregar a productos
+                    productosVM.Add(
+                        new IndexVM
+                        {
+                            nombre = producto.nombre,
+                            precio = registroMasReciente.precio,
+                            unidad = producto.nombreUnidad,
+                            fecha = registroMasReciente.creacion,
+                            tienda = registroMasReciente.nombreTienda,
+                            provincia = registroMasReciente.nombreProvincia,
+                            canton = registroMasReciente.nombreCanton
+                        });
+                }
+            }
         }
     }
 }
---
+++ source/LoCoMPro/ViewModels/Busqueda/IndexVM.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoCoMPro.ViewModels.Busqueda
+{
+    public class IndexVM
+    {
+        // Nombre
+        [Display(Name = "Nombre")]
+        public required string nombre { get; set; }
+        // Precio
+        [Display(Name = "Precio")]
+        public required decimal precio { get; set; }
+        // Unidad
+        [Display(Name = "Unidad")]
+        public required string unidad { get; set; }
+        // Fecha
+        [Display(Name = "Fecha")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public required DateTime fecha { get; set; }
+        // Establecimiento
+        [Display(Name = "Tienda")]
+        public required string tienda { get; set; }
+        // Provincia
+        [Display(Name = "Provincia")]
+        public required string provincia { get; set; }
+        // Canton
+        [Display(Name = "Cantón")]
+        public required string canton { get; set; }
+        // Marca
+        [Display(Name = "Marca")]
+        public string? marca { get; set; }
+    }
+}
