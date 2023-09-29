accf33d583c49c8ff7de9e49e8f0b83948490a7f
ENRIQUE GUILLERMO VILCHEZ LIZANO <enrique.vilchezlizano@ucr.ac.cr>
2023-09-28 20:19:16 -0600

    Modificada barra de busqueda y agregado modelo inicial.

--- source/LoCoMPro/Pages/Busqueda/Index.cshtml.cs
+++ source/LoCoMPro/Pages/Busqueda/Index.cshtml.cs
@@ -1,12 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LoCoMPro.Models;
+
 
 namespace LoCoMPro.Pages.Busqueda
 {
-    public class IndexModel : PageModel
+    public class IndexVMModel : PageModel
     {
+        private readonly LoCoMPro.Data.LoCoMProContext _context;
+        private readonly IConfiguration _configuration;
+
+
+        public IndexVMModel(LoCoMPro.Data.LoCoMProContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        // Busquedas
+        [BindProperty(SupportsGet = true)]
+        public string? producto { get; set; }
+       
         public void OnGet()
         {
+            
         }
     }
 }
+
