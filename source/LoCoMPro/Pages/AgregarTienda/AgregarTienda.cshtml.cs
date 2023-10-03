4427d53c74ed9afdb849c97c773ed8c2209bc6a8
Angie Solis <angie.solismanzano@ucr.ac.cr>
2023-10-02 21:38:24 -0600

    Métodos para verificar datos obtenidos del usuario en agregar tienda

--- source/LoCoMPro/Pages/AgregarTienda/AgregarTienda.cshtml.cs
+++ source/LoCoMPro/Pages/AgregarTienda/AgregarTienda.cshtml.cs
@@ -1,29 +1,150 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using LoCoMPro.ViewModels.Tienda;
+using LoCoMPro.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoCoMPro.Pages.AgregarTienda
 {
     public class AgregarTiendaModel : PageModel
     {
+        // Distrito
+        [BindProperty]
+        public required string Distrito { get; set; }
+
+        // Cantón
+        [BindProperty]
+        public required string Canton { get; set; }
+
+        // Provincia
+        [BindProperty]
+        public required string Provincia { get; set; }
+
+        // Nombre
+        [BindProperty]
+        public required string Nombre { get; set; }
+
         // View model de tienda
         [BindProperty]
         public required AgregarTiendaVM Tienda { get; set; }
 
-        public void OnGet()
-        {
-        }
-        public void OnPostSiguiente()
-        {
-            // TODO(Angie): hacer acción de seguir a agregar producto
+        // Lista para guardar las provincias
+        public List<Provincia> ListaProvincias { get; set; }
 
+        // Contexto
+        private readonly LoCoMPro.Data.LoCoMProContext contexto;
+
+        //Constructor
+        public AgregarTiendaModel(LoCoMPro.Data.LoCoMProContext contexto)
+        {
+            this.contexto = contexto;
+            this.Tienda = new AgregarTiendaVM
+            {  // Crea una tienda vacía para trabajar con ella más adelante
+                nombre = " ",
+                nombreDistrito = " ",
+                nombreCanton = " ",
+                nombreProvincia = " "
+            };
+
+            // Crea una lista para guardar las provincias
+            this.ListaProvincias = new List<Provincia>();
+        }
+
+        // OnGet de la página
+        public IActionResult OnGet()
+        {
+            // Cargar toda la información de provincias de la base de datos
+            this.ListaProvincias = this.contexto.Provincias.ToList();
+
+            return Page();
+        }
+
+        // Acción al presionar siguiente
+        public IActionResult OnPostSiguiente()
+        {
+            // Leer datos del usuario
+            if (!AnalizarDatos())
+            {
+                // Los datos no son válidos
+                return Page();
+            }
+
+            // Preparar datos para la siguiente ventana
+            AgregarDatosTienda();
+
+            // Redirigir a la siguiente ventana de agregar producto
+            return RedirectToPage("/Index");
+
+            // TODO(Los Pinwinos): actualizar para ir a la página de agregar producto
+            // return RedirectToPage("/AgregarProducto/AgregarProd");
+        }
+
+        // Método para leer los datos que el usuario ingresó
+        private bool AnalizarDatos()
+        {
+            // Guarda los nuevos datos en la tienda
+            this.Tienda.nombreProvincia = Provincia;
+            this.Tienda.nombreCanton = Canton;
+            this.Tienda.nombreDistrito = Distrito;
+            this.Tienda.nombre = Nombre;
+
+            // Se verifica si la tienda es válida
+            return VerificarValida();
+        }
+
+        // Método para verificar validez de la tienda
+        private bool VerificarValida()
+        {
+            // Revisa si existe una tienda con ese nombre en el distrito, cantón y provincia indicados
+            var tiendaExistenteEnDistrito = contexto.Tiendas.FirstOrDefault
+                (p => p.nombre == this.Tienda.nombre
+                && p.nombreDistrito == this.Tienda.nombreDistrito
+                && p.nombreCanton == this.Tienda.nombreCanton
+                && p.nombreProvincia == this.Tienda.nombreProvincia);
+
+            if (tiendaExistenteEnDistrito == null)
+            {  // La tienda no existe y el nombre está disponible
+                CrearNuevaTienda();
+            }
+
+            // La tienda ya existe
+            return true;
+        }
+
+        // Se agrega una nueva tienda en la base de datos
+        private void CrearNuevaTienda()
+        {
+            // Si es nueva, se debe crear
+            var nuevaTienda = new Tienda
+            {
+                nombre = this.Tienda.nombre,
+                nombreDistrito = this.Tienda.nombreDistrito,
+                nombreCanton = this.Tienda.nombreCanton,
+                nombreProvincia = this.Tienda.nombreProvincia
+            };
+
+            // Agrega la nueva tienda a la base de datos
+            this.contexto.Add(nuevaTienda);
+            this.contexto.SaveChanges();
+        }
+
+        // Datos para siguiente ventana
+        private void AgregarDatosTienda()
+        {
+            // Guarda los datos en TempData para que puedan ser accesados después
+            TempData["nombreTienda"] = this.Tienda.nombre;
+            TempData["provinciaTienda"] = this.Tienda.nombreProvincia;
+            TempData["cantonTienda"] = this.Tienda.nombreCanton;
+            TempData["distritoTienda"] = this.Tienda.nombreDistrito;
         }
 
         // Acción al presionar cancelar
         public IActionResult OnPostCancelar()
         {
-            // Cuando se cancela agregar un producto, se devuelve a la pantalla principal
-            return RedirectToPage("/Home/Index");
+            return RedirectToPage("/Index");
+
+            // TODO(Los Pinwinos): actualizar para que se dirija a la página de inicio
+            // return RedirectToPage("/Home/Index");
         }
     }
 }
