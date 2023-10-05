85bd6a4e50b474c3e69cdc2ada7197dea6552bbe
ENRIQUE GUILLERMO VILCHEZ LIZANO <enrique.vilchezlizano@ucr.ac.cr>
2023-10-05 14:31:33 -0600

    Agregada página de Home inicial, con botón de búsqueda avanzada sin funcionamiento.

---
+++ source/LoCoMPro/Pages/Home/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection.Metadata;
+
+namespace LoCoMPro.Pages.Home
+{
+    public class HomeModel : PageModel
+    {
+        // Busqueda
+        [BindProperty(SupportsGet =true)]
+        public string? producto { get; set; }
+        // On GET
+        public IActionResult OnGet()
+        {
+            if (!string.IsNullOrWhiteSpace(producto)) { 
+                // Redireccionar
+                return RedirectToPage("/Busqueda/Index", new { nombreProducto = this.producto });
+            } else
+            {
+                return Page();
+            }
+        }
+    }
+}
