3bb8191b149453941596c4d8d61fd1b98613352f
ENRIQUE GUILLERMO VILCHEZ LIZANO <enrique.vilchezlizano@ucr.ac.cr>
2023-10-19 13:11:58 -0600

    Se agregó una interfaz de un objeto buscador de la base de datos y un buscador especializado para la página de búsqueda

---
+++ source/LoCoMPro/Utils/Interfaces/IBuscador.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LoCoMPro.Utils.Interfaces
+{
+    // Buscador genérico
+    public interface IBuscador<Tipo>
+    {
+        // Método genérico de búsqueda
+        public IQueryable<Tipo> buscar();
+    }
+}
