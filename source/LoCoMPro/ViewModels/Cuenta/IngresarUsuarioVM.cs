6ba0a9572f65eb9a7fe05ee6f780c402e304b467
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2023-10-18 22:14:45 -0600

    El mensaje de error en la página de ingresar ahora se muestra correctamente.

--- source/LoCoMPro/ViewModels/Cuenta/IngresarUsuarioVM.cs
+++ source/LoCoMPro/ViewModels/Cuenta/IngresarUsuarioVM.cs
@@ -6,17 +6,10 @@ namespace LoCoMPro.ViewModels.Cuenta
     public class IngresarUsuarioVM
     {
         // Nombre de usuario
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Debe incluir un nombre de usuario")]
-        [StringLength(20, MinimumLength = 5, ErrorMessage = "Formato de usuario inválido")]
         public string nombreDeUsuario { get; set; }
 
         // Contraseña
         [DataType(DataType.Password)]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Debe incluir una contraseña")]
-        [StringLength(20, MinimumLength = 8)]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-+_=*./\\%$#@!¡¿?()~])[-a-zA-Z\d+_=*./\\%$#@!¡¿?()~]+$",
-            ErrorMessage =
-            "Formato de contraseña inválido")]
         public string contrasena { get; set; }
     }
 }
