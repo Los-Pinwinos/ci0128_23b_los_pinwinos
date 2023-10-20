f2a5880ab9000d1776ea72bbd0eef028f56bc573
ENRIQUE GUILLERMO VILCHEZ LIZANO <enrique.vilchezlizano@ucr.ac.cr>
2023-10-20 15:17:26 -0600

    Se agregó un buscador especializado para búsqueda avanzada en su respectivo modelo de página

--- source/LoCoMPro/Pages/Busqueda/Avanzada.cshtml.cs
+++ source/LoCoMPro/Pages/Busqueda/Avanzada.cshtml.cs
@@ -2,7 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LoCoMPro.Data;
 using LoCoMPro.ViewModels.Busqueda;
-
+using LoCoMPro.Utils.Busqueda;
+using LoCoMPro.Utils.Interfaces;
 
 namespace LoCoMPro.Pages.Busqueda
 {
@@ -35,130 +36,33 @@ namespace LoCoMPro.Pages.Busqueda
         }
 
         // On GET avanzado
-        public async Task<IActionResult> OnGetBuscarAvanzadoAsync(int? indicePagina
-            , string? nombreProducto, string? filtroProducto
-            , string? nombreMarca, string? filtroMarca
-            , string? nombreProvincia, string? filtroProvincia
-            , string? nombreCanton, string? filtroCanton
-            , string? nombresProvincias, string? filtrosProvincias
-            , string? nombresCantones, string? filtrosCantones
-            , string? nombresTiendas, string? filtrosTiendas
-            , string? nombresMarcas, string? filtrosMarcas
-            , string? columnaOrdenado, string? sentidoOrdenado)
+        public IActionResult OnGetBuscarAvanzado(
+            string? nombreProducto
+            , string? nombreMarca
+            , string? nombreProvincia
+            , string? nombreCanton)
         {
+            if ((!string.IsNullOrEmpty(nombreProducto) 
+                || !string.IsNullOrEmpty(nombreMarca)
+                || !string.IsNullOrEmpty(nombreProvincia)
+                || !string.IsNullOrEmpty(nombreCanton))
+                && this.contexto.Productos != null)
+            {
+                // Asignar variables
+                producto = string.IsNullOrEmpty(nombreProducto) ? "" : nombreProducto;
+                marca = string.IsNullOrEmpty(nombreMarca) ? "" : nombreMarca;
+                provincia = string.IsNullOrEmpty(nombreProvincia) ? "" : nombreProvincia;
+                canton = string.IsNullOrEmpty(nombreProvincia) ? "" : nombreCanton;
+                // Configurar buscador
+                IBuscador<BusquedaVM> buscador = new BuscadorDeBusquedaAvanzada(this.contexto, nombreProducto, nombreMarca, nombreProvincia, nombreCanton);
+                // Consultar la base de datos
+                IQueryable<BusquedaVM> busqueda = buscador.buscar();
+                // Cargar filtros
+                this.cargarFiltros(busqueda);
+                // Asignar data de JSON
+                this.resultadosBusqueda = Newtonsoft.Json.JsonConvert.SerializeObject(busqueda.ToList());
+            }
             return Page();
-        }
-
-        // Verificar parámetros
-        private int? verificarParametrosAvanzados(int? indicePagina
-            , string? nombreMarca, string? filtroMarca
-            , string? nombreProvincia, string? filtroProvincia
-            , string? nombreCanton, string? filtroCanton)
-        {
-            // Revisar si hay que regresar numero de página
-            if (!string.IsNullOrEmpty(nombreMarca))
-            {
-                indicePagina = 1;
-            }
-            else
-            {
-                nombreMarca = filtroMarca;
-            }
-            this.marca = nombreMarca;
-
-            if (!string.IsNullOrEmpty(nombreProvincia))
-            {
-                indicePagina = 1;
-            }
-            else
-            {
-                nombreProvincia = filtroProvincia;
-            }
-            this.provincia = nombreProvincia;
-
-            if (!string.IsNullOrEmpty(nombreCanton))
-            {
-                indicePagina = 1;
-            }
-            else
-            {
-                nombreCanton = filtroCanton;
-            }
-            this.canton = nombreCanton;
-
-            return indicePagina;
-        }
-
-        // Sobrecarga de buscar productos
-        new protected IQueryable<BusquedaVM> buscarProductos()
-        {
-            IQueryable<BusquedaVM> productosIQ = contexto.Registros
-                .Include(r => r.producto)
-                .GroupBy(r => new
-                {
-                    r.productoAsociado,
-                    r.nombreTienda,
-                    r.nombreProvincia,
-                    r.nombreCanton,
-                    r.nombreDistrito
-                })
-                .Select(group => new BusquedaVM
-                {
-                    nombre = group.Key.productoAsociado,
-                    precio = group.OrderByDescending(item => item.creacion).First().precio,
-                    unidad = group.First().producto.nombreUnidad,
-                    fecha = group.OrderByDescending(item => item.creacion).First().creacion,
-                    tienda = group.Key.nombreTienda,
-                    provincia = group.Key.nombreProvincia,
-                    canton = group.Key.nombreCanton,
-                    marca = !string.IsNullOrEmpty(group.First().producto.marca) ?
-                        group.First().producto.marca : "Sin marca"
-                });
-      
-            // Retornar busqueda
-            return productosIQ;
-        }
-
-        // Buscar por marca
-        private IQueryable<BusquedaVM> buscarMarca(IQueryable<BusquedaVM> productosIQ)
-        {
-            // Ver si se usa el nombre de busqueda
-            if (!string.IsNullOrEmpty(marca))
-            {
-                return productosIQ.Where(r => r.marca.Contains(marca));
-            }
-            else
-            {
-                return productosIQ;
-            }
-        }
-
-        // Buscar por provincia
-        private IQueryable<BusquedaVM> buscarProvincia(IQueryable<BusquedaVM> productosIQ)
-        {
-            // Ver si se usa el nombre de busqueda
-            if (!string.IsNullOrEmpty(provincia))
-            {
-                return productosIQ.Where(r => r.provincia.Contains(provincia));
-            }
-            else
-            {
-                return productosIQ;
-            }
-        }
-
-        // Buscar por canton
-        private IQueryable<BusquedaVM> buscarCanton(IQueryable<BusquedaVM> productosIQ)
-        {
-            // Ver si se usa el nombre de busqueda
-            if (!string.IsNullOrEmpty(canton))
-            {
-                return productosIQ.Where(r => r.canton.Contains(canton));
-            }
-            else
-            {
-                return productosIQ;
-            }
         }
     }
 }
