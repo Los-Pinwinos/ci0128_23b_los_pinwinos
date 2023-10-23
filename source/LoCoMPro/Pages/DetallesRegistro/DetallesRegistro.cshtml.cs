d2ab53d6b135263b5046fbc988730112a94b20bc
Angie Solis <angie.solismanzano@ucr.ac.cr>
2023-10-23 11:54:13 -0600

    Página inicial para ver los detalles del registro

---
+++ source/LoCoMPro/Pages/DetallesRegistro/DetallesRegistro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages.DetallesRegistro
+{
+    public class DetallesRegistroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
