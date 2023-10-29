d2a3cd66f7f4209c96f8a4adba75eb29722b4f56
Luis <luisdi2509@gmail.com>
2023-10-29 08:52:20 -0600

    Lógica de mostrado de flechas de ordenamiento en búsqueda normal implementada

--- source/LoCoMPro/wwwroot/js/Pages/Busqueda/ordenador.js
+++ source/LoCoMPro/wwwroot/js/Pages/Busqueda/ordenador.js
@@ -45,15 +45,19 @@ class OrdenadorDeBusqueda {
 
     // Métodos de ordenamiento para propiedades específicas.
     ordenarAscendente(resultadosIQ) {
+        this.esconderFlechas();
         switch (this.ordenado) {
             case 'precio':
                 resultadosIQ = resultadosIQ.sort((a, b) => a.precio - b.precio);
+                document.getElementById("DescendentePrecio").hidden = false;
                 break;
             case 'provincia':
                 resultadosIQ = resultadosIQ.sort((a, b) => a.provincia.localeCompare(b.provincia));
+                document.getElementById("DescendenteProvincia").hidden = false;
                 break;
             case 'canton':
                 resultadosIQ = resultadosIQ.sort((a, b) => a.canton.localeCompare(b.canton));
+                document.getElementById("DescendenteCanton").hidden = false;
                 break;
             default:
                 break;
@@ -63,15 +67,19 @@ class OrdenadorDeBusqueda {
 
     // Método de ordenamiento descendente para propiedades específicas.
     ordenarDescendente(resultadosIQ) {
+        this.esconderFlechas();
         switch (this.ordenado) {
             case 'precio':
                 resultadosIQ = resultadosIQ.sort((a, b) => b.precio - a.precio);
+                document.getElementById("AscendentePrecio").hidden = false;
                 break;
             case 'provincia':
                 resultadosIQ = resultadosIQ.sort((a, b) => b.provincia.localeCompare(a.provincia));
+                document.getElementById("AscendenteProvincia").hidden = false;
                 break;
             case 'canton':
                 resultadosIQ = resultadosIQ.sort((a, b) => b.canton.localeCompare(a.canton));
+                document.getElementById("AscendenteCanton").hidden = false;
                 break;
             default:
                 break;
@@ -79,8 +87,31 @@ class OrdenadorDeBusqueda {
         return resultadosIQ;
     }
 
+    // Método para esconder todas las flechas
+    esconderFlechas() {
+        document.getElementById("DescendenteNombre").hidden = true;
+        document.getElementById("AscendenteNombre").hidden = true;
+        document.getElementById("DescendenteCategoria").hidden = true;
+        document.getElementById("AscendenteCategoria").hidden = true;
+        document.getElementById("DescendenteMarca").hidden = true;
+        document.getElementById("AscendenteMarca").hidden = true;
+        document.getElementById("DescendentePrecio").hidden = true;
+        document.getElementById("AscendentePrecio").hidden = true;
+        document.getElementById("DescendenteUnidad").hidden = true;
+        document.getElementById("AscendenteUnidad").hidden = true;
+        document.getElementById("DescendenteFecha").hidden = true;
+        document.getElementById("AscendenteFecha").hidden = true;
+        document.getElementById("DescendenteTienda").hidden = true;
+        document.getElementById("AscendenteTienda").hidden = true;
+        document.getElementById("DescendenteProvincia").hidden = true;
+        document.getElementById("AscendenteProvincia").hidden = true;
+        document.getElementById("DescendenteCanton").hidden = true;
+        document.getElementById("AscendenteCanton").hidden = true;
+    }
+
     // Un método para restablecer el estado de "usado" del ordenador.
     resetearUso() {
         this.usado = false;
+        this.esconderFlechas();
     }
 }
