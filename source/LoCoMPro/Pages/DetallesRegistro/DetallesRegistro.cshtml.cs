6fc251afb215e141ba8fefd91d26411d132cbcd9
Angie Solis <angie.solismanzano@ucr.ac.cr>
2023-10-29 00:15:02 -0600

    Detalles Registros obtiene los datos de la base de datos para el registro seleccionado

--- source/LoCoMPro/Pages/DetallesRegistro/DetallesRegistro.cshtml.cs
+++ source/LoCoMPro/Pages/DetallesRegistro/DetallesRegistro.cshtml.cs
@@ -2,6 +2,7 @@ using LoCoMPro.Data;
 using LoCoMPro.ViewModels.DetallesRegistro;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoCoMPro.Pages.DetallesRegistro
 {
@@ -23,8 +24,38 @@ namespace LoCoMPro.Pages.DetallesRegistro
             };
         }
 
-        public void OnGet()
+        public void OnGet(string fechaHora, string usuario, string producto)
         {
+
+            
+
+            DateTime fecha = DateTime.Parse(fechaHora);
+
+            System.Diagnostics.Debug.WriteLine("fecha: " + fecha.ToString() + " usuario: " + usuario + " producto: " + producto);
+
+            var detallesIQ = contexto.Registros
+                .Include(r => r.producto)
+                .Include(r => r.fotografias)
+                .Where(r => r.creacion.Equals(fecha) && r.usuarioCreador.Equals(usuario))
+                .Select(r => new DetallesRegistroVM
+                {
+                    usuarioCreador = usuario,
+                    precio = r.precio,
+                    calificacion = r.calificacion,
+                    descripcion = r.descripcion,
+                    productoAsociado = producto,
+                    nombreUnidad = r.producto.nombreUnidad,
+                    fotografias = r.fotografias
+                }).ToList();
+
+
+            System.Diagnostics.Debug.WriteLine(detallesIQ.Count);
+
+
+            this.registro = detallesIQ.FirstOrDefault();
+            
+
+            // TODO(Angie): cantidad de calificaciones de registros
         }
     }
 }
