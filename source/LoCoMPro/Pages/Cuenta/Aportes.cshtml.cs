bbaa12de66047d6fd29d032949adb2b5204f1f6d
kdaniel1652 <kennethvillalobos16@gmail.com>
2023-10-30 18:53:41 -0600

    Agregada visualización de aportes en la página respectiva en conjunto con su paginación.

---
+++ source/LoCoMPro/Pages/Cuenta/Aportes.cshtml.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using LoCoMPro.Data;
+using LoCoMPro.ViewModels.Busqueda;
+using LoCoMPro.Utils.Buscadores;
+using LoCoMPro.Utils.Interfaces;
+using Newtonsoft.Json;
+
+namespace LoCoMPro.Pages.Cuenta
+{
+    public class ModeloAportes : PageModel
+    {
+        protected readonly LoCoMProContext contexto;
+        protected readonly IConfiguration configuracion;
+
+        // Constructor
+        public ModeloAportes(LoCoMProContext contexto, IConfiguration configuracion)
+        {
+            this.contexto = contexto;
+            this.configuracion = configuracion;
+            // Inicializar
+            this.Inicializar();
+        }
+
+        public string? aportes { get; set; }
+        // Visual
+        [BindProperty]
+        public IList<string> provinciasV { get; set; } = default!;
+        public IList<string> cantonesV { get; set; } = default!;
+        public IList<string> tiendasV { get; set; } = default!;
+        public IList<string?> marcasV { get; set; } = default!;
+        public BusquedaVM productoVM { get; set; } = default!;
+
+        // Paginación
+        public int paginaDefault { get; set; }
+        public int resultadosPorPagina { get; set; }
+
+        // Inicializar atributos
+        public void Inicializar()
+        {
+
+            // Inicializar datos de vista
+            this.provinciasV = new List<string>();
+            this.cantonesV = new List<string>();
+            this.tiendasV = new List<string>();
+            this.marcasV = new List<string?>();
+
+            this.productoVM = new BusquedaVM
+            {
+                nombre = ""
+                                                ,
+                canton = ""
+                                                ,
+                fecha = new DateTime()
+                                                ,
+                precio = 0
+                                                ,
+                marca = ""
+                                                ,
+                provincia = ""
+                                                ,
+                tienda = ""
+                                                ,
+                unidad = ""
+                                                ,
+                categoria = ""
+            };
+            this.paginaDefault = 1;
+            this.resultadosPorPagina = this.configuracion.GetValue("TamPagina", 4);
+        }
+
+        // ON GET buscar
+        public IActionResult OnGet()
+        {
+            if (User.Identity == null || !User.Identity.IsAuthenticated)
+            {
+                // Establece mensaje para redireccionar si el usuario no está ingresado
+                // en el sistema
+                ViewData["MensajeRedireccion"] = "Por favor ingrese al sistema.";
+            } else
+            {
+                // Configurar buscador
+                IBuscador<BusquedaVM> buscador = new BuscadorDeAportes(this.contexto, User.Identity.Name);
+                // Consultar la base de datos
+                IQueryable<BusquedaVM> busqueda = buscador.buscar();
+                // Asignar data de JSON
+                this.aportes = JsonConvert.SerializeObject(busqueda.ToList());
+            }
+            return Page();
+        }
+    }
+}
---
+++ source/LoCoMPro/wwwroot/js/Pages/Cuenta/cliente.js
@@ -0,0 +1,263 @@
+﻿function agregarSeparador(numero) {
+    var textoNum = numero.toString();
+    // Expresión regular para agregar un separador cada 3 dígitos
+    textoNum = textoNum.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    return textoNum;
+}
+
+// Formatear fecha
+function formatearFecha(fecha) {
+    const opciones = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return fecha.toLocaleDateString('es-ES', opciones);
+}
+
+// Paginar
+function paginar(numeroPagina = productosVM.IndicePagina) {
+    return paginador.paginar(resultados, numeroPagina);
+}
+
+function renderizarPaginacion() {
+    // Renderizar los botones de Siguiente Pagina y Pagina Anterior
+    renderizarBotonesSiguienteAnterior();
+
+    var paginacionContenedor = document.getElementById("TextoPaginacion");
+
+    // Renderizar los numeros de en medio mostrados en la barra de paginacion
+    var limites = renderizarNumerosPaginaIntermedios(paginacionContenedor);
+    renderizarPrimerNumeroPagina(paginacionContenedor, limites.paginaInicial);
+    renderizarUltimoNumeroPagina(paginacionContenedor, limites.paginaFinal);
+}
+
+function renderizarUltimoNumeroPagina(paginacionContenedor, paginaFinal) {
+    if (paginaFinal < productosVM.PaginasTotales) {
+        const finalElipsis = document.createElement("span");
+        // Agregar ... a la ultima pagina si fuera necesario
+
+        if (paginaFinal !== productosVM.PaginasTotales - 1) finalElipsis.textContent = " ... ";
+        else finalElipsis.textContent = " ";
+        paginacionContenedor.appendChild(finalElipsis);
+
+        const linkUltimaPagina = document.createElement("span");
+        linkUltimaPagina.textContent = productosVM.PaginasTotales;
+        linkUltimaPagina.classList.add("pagina-seleccionable");
+
+        linkUltimaPagina.addEventListener("click", function () {
+            pasarPagina(productosVM.PaginasTotales);
+        });
+        paginacionContenedor.appendChild(linkUltimaPagina);
+    }
+}
+
+function renderizarPrimerNumeroPagina(paginacionContenedor, paginaInicial) {
+    if (paginaInicial > 1) {
+        const inicioElipsis = document.createElement("span");
+
+        // Agregar ... a la primera pagina si fuera necesario
+        if (paginaInicial !== 2) inicioElipsis.textContent = " ... ";
+        else inicioElipsis.textContent = " ";
+
+        paginacionContenedor.insertBefore(inicioElipsis, paginacionContenedor.firstChild);
+
+        const linkPrimeraPagina = document.createElement("span");
+        linkPrimeraPagina.textContent = "1";
+        linkPrimeraPagina.classList.add("pagina-seleccionable");
+
+        linkPrimeraPagina.addEventListener("click", function () {
+            pasarPagina(1);
+        });
+        paginacionContenedor.insertBefore(linkPrimeraPagina, inicioElipsis);
+    }
+}
+
+function renderizarNumerosPaginaIntermedios(paginacionContenedor) {
+
+    paginacionContenedor.innerHTML = "";
+
+    const numeroDeLinksDePaginas = 5;
+
+    let paginaInicial = Math.max(1, productosVM.IndicePagina - Math.floor(numeroDeLinksDePaginas / 2));
+    let paginaFinal = Math.min(productosVM.PaginasTotales, paginaInicial + numeroDeLinksDePaginas - 1);
+
+    if (paginaFinal - paginaInicial + 1 < numeroDeLinksDePaginas) {
+        paginaInicial = Math.max(1, paginaFinal - numeroDeLinksDePaginas + 1);
+    }
+
+    for (let pagina = paginaInicial; pagina <= paginaFinal; pagina++) {
+        const paginaLink = document.createElement("span");
+        paginaLink.textContent = pagina;
+
+        if (pagina === productosVM.IndicePagina) {
+            paginaLink.classList.add("pagina");
+        } else {
+            paginaLink.classList.add("pagina-seleccionable");
+            paginaLink.addEventListener("click", function () {
+                pasarPagina(pagina);
+            });
+        }
+
+        paginacionContenedor.appendChild(paginaLink);
+        if (pagina !== paginaFinal) {
+            var espacio = document.createElement("span");
+            espacio.textContent = " ";
+            paginacionContenedor.appendChild(espacio);
+        }
+    }
+
+    return { paginaInicial, paginaFinal };
+}
+
+function renderizarBotonesSiguienteAnterior() {
+    var botonPaginaPrevia = document.getElementById("PaginaPrevia");
+    var botonSinPaginaPrevia = document.getElementById("SinPaginaPrevia");
+    var botonPaginaSiguiente = document.getElementById("PaginaSiguiente");
+    var botonSinPaginaSiguiente = document.getElementById("SinPaginaSiguiente");
+
+    if (productosVM.TienePaginaPrevia) {
+        botonPaginaPrevia.style.display = "block";
+        botonSinPaginaPrevia.style.display = "none";
+    } else {
+        botonPaginaPrevia.style.display = "none";
+        botonSinPaginaPrevia.style.display = "block";
+    }
+
+    if (productosVM.TieneProximaPagina) {
+        botonPaginaSiguiente.style.display = "block";
+        botonSinPaginaSiguiente.style.display = "none";
+    } else {
+        botonPaginaSiguiente.style.display = "none";
+        botonSinPaginaSiguiente.style.display = "block";
+    }
+}
+
+// Renderizar tabla
+function renderizarTabla(datos) {
+    // Obtener la tabla
+    var cuerpoTabla = document.getElementById("CuerpoResultados");
+
+    // Limpiar el contenido existente
+    cuerpoTabla.innerHTML = "";
+
+    // Iterar
+    for (var dato in datos) {
+        var row = document.createElement("tr");
+        // result-row se utiliza para redirigir los datos de la fila selecionada a la página de VerRegistros
+        row.classList.add("result-row")
+
+        if (typeof datos[dato].nombre !== 'undefined' && datos[dato].nombre !== "") {
+            var divNombre = document.createElement("div");
+            divNombre.className = "contenidoCeldaNombre";
+            divNombre.textContent = datos[dato].nombre;
+            var nombreCelda = document.createElement("td");
+            nombreCelda.setAttribute('data-tooltip', datos[dato].nombre);
+            nombreCelda.appendChild(divNombre);
+
+            var nombreCell = document.createElement("td");
+            nombreCell.textContent = datos[dato].nombre;
+
+            var divCategoria = document.createElement("div");
+            divCategoria.className = "contenidoCeldaCategoria";
+            divCategoria.textContent = datos[dato].categoria;
+            var categoriaCelda = document.createElement("td");
+            categoriaCelda.setAttribute('data-tooltip', datos[dato].categoria);
+            categoriaCelda.appendChild(divCategoria);
+
+            var divMarca = document.createElement("div");
+            divMarca.className = "contenidoCeldaMarca";
+            divMarca.textContent = datos[dato].marca;
+            var marcaCelda = document.createElement("td");
+            marcaCelda.setAttribute('data-tooltip', datos[dato].marca);
+            marcaCelda.appendChild(divMarca);
+
+            var divPrecio = document.createElement("div");
+            divPrecio.className = "contenidoCeldaPrecio";
+            var precioArreglado = "₡" + agregarSeparador(parseFloat(datos[dato].precio));
+            divPrecio.textContent = precioArreglado;
+            var precioCelda = document.createElement("td");
+            precioCelda.setAttribute('data-tooltip', precioArreglado);
+            precioCelda.appendChild(divPrecio);
+
+            var divUnidad = document.createElement("div");
+            divUnidad.className = "contenidoCeldaUnidad";
+            divUnidad.textContent = datos[dato].unidad;
+            var unidadCelda = document.createElement("td");
+            unidadCelda.setAttribute('data-tooltip', datos[dato].unidad);
+            unidadCelda.appendChild(divUnidad);
+
+            var divFecha = document.createElement("div");
+            divFecha.className = "contenidoCeldaFecha";
+            divFecha.textContent = formatearFecha(new Date(datos[dato].fecha));
+            var fechaCelda = document.createElement("td");
+            var contenidoFecha = datos[dato].fecha[8] + datos[dato].fecha[9] + "/"
+                + datos[dato].fecha[5] + datos[dato].fecha[6] + "/" + datos[dato].fecha[0] + datos[dato].fecha[1]
+                + datos[dato].fecha[2] + datos[dato].fecha[3];
+            fechaCelda.setAttribute('data-tooltip', contenidoFecha);
+            fechaCelda.appendChild(divFecha);
+
+            var divTienda = document.createElement("div");
+            divTienda.className = "contenidoCeldaTienda";
+            divTienda.textContent = datos[dato].tienda;
+            var tiendaCelda = document.createElement("td");
+            tiendaCelda.setAttribute('data-tooltip', datos[dato].tienda);
+            tiendaCelda.appendChild(divTienda);
+
+            var divProvincia = document.createElement("div");
+            divProvincia.className = "contenidoCeldaProvincia";
+            divProvincia.textContent = datos[dato].provincia;
+            var provinciaCelda = document.createElement("td");
+            provinciaCelda.setAttribute('data-tooltip', datos[dato].provincia);
+            provinciaCelda.appendChild(divProvincia);
+
+            var divCanton = document.createElement("div");
+            divCanton.className = "contenidoCeldaCanton";
+            divCanton.textContent = datos[dato].canton;
+            var cantonCelda = document.createElement("td");
+            cantonCelda.setAttribute('data-tooltip', datos[dato].canton);
+            cantonCelda.appendChild(divCanton);
+
+            // Agregar celdas a fila
+            row.appendChild(nombreCelda);
+            row.appendChild(categoriaCelda);
+            row.appendChild(marcaCelda);
+            row.appendChild(precioCelda);
+            row.appendChild(unidadCelda);
+            row.appendChild(fechaCelda);
+            row.appendChild(tiendaCelda);
+            row.appendChild(provinciaCelda);
+            row.appendChild(cantonCelda);
+
+           
+            // Agregar celdas cuerpo
+            cuerpoTabla.appendChild(row);
+
+        }
+    }
+}
+
+// Redirecionar a ver registros
+document.addEventListener("click", function () {
+    const rows = document.querySelectorAll(".result-row");
+
+    rows.forEach(row => {
+        row.addEventListener("click", function () {
+            const productName = row.querySelector("td:nth-child(1)").textContent;      // Columna 1
+            const categoriaNombre = row.querySelector("td:nth-child(2)").textContent;  // Columna 2
+            const marcaNombre = row.querySelector("td:nth-child(3)").textContent;      // Columna 3
+            const unidadNombre = row.querySelector("td:nth-child(5)").textContent;     // Columna 5
+            const tiendaNombre = row.querySelector("td:nth-child(7)").textContent;     // Columna 7
+            const provinciaNombre = row.querySelector("td:nth-child(8)").textContent;  // Columna 8
+            const cantonNombre = row.querySelector("td:nth-child(9)").textContent;     // Columna 9
+
+            window.location.href = `/VerRegistros/VerRegistros?productName=${encodeURIComponent(productName)}&categoriaNombre=${encodeURIComponent(categoriaNombre)}&marcaNombre=${encodeURIComponent(marcaNombre)}&unidadNombre=${encodeURIComponent(unidadNombre)}&tiendaNombre=${encodeURIComponent(tiendaNombre)}&provinciaNombre=${encodeURIComponent(provinciaNombre)}&cantonNombre=${encodeURIComponent(cantonNombre)}`;
+        });
+    });
+});
+
+// Pasar pagina
+function pasarPagina(numeroPagina) {
+    // Paginar
+    productosVM = paginar(numeroPagina);
+    // Renderizar
+    renderizarPaginacion();
+    renderizarTabla(productosVM);
+    window.scrollTo(0, 0);
+}
