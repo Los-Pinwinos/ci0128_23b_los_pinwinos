0bd7acf8487ad44b57d74d554a5e0a9a6109324f
ENRIQUE GUILLERMO VILCHEZ LIZANO <enrique.vilchezlizano@ucr.ac.cr>
2023-11-04 12:10:07 -0600

    Se agregó un mock generalizado de mock de un contexto de la base de datos

---
+++ test/LoCoMProTests/Mocks/MockDeContexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace LoCoMProTests.Mocks
+{
+    public class MockDeContexto<TipoContexto> : MockWrapper<TipoContexto> where TipoContexto : DbContext
+    {
+        public MockDeContexto() : base() { }
+
+        // Agrega una instancia del modelo al conjunto de datos del Mock
+        public void ConfigurarParaInstanciasDeModelo<TipoModelo>
+            (Expression<Func<TipoContexto, DbSet<TipoModelo>>> expresionDeModeloEnContexto,
+            DbSet<TipoModelo> instancias) where TipoModelo : class
+        {
+
+            this.mock.Setup(expresionDeModeloEnContexto).Returns(instancias);
+
+        }
+    }
+}
