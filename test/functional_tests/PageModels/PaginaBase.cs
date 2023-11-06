f5f2e73440c3b1cf720f3d9fd34d2b51ddd0f8e4
ENRIQUE GUILLERMO VILCHEZ LIZANO <enrique.vilchezlizano@ucr.ac.cr>
2023-11-06 14:49:19 -0600

    Se agregaron pruebas funcionales de la búsqueda en la página Home y filtrado de búsqueda

---
+++ test/functional_tests/PageModels/PaginaBase.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+
+namespace LoCoMProTestFuncionales.PageModels
+{
+    // Página base
+    public class PaginaBase
+    {
+        protected IWebDriver driver;
+
+        public PaginaBase(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        virtual public string ObtenerURL()
+        {
+            return "http://localhost:5150";
+        }
+
+        public void EsperarMilisegundos(int milisegundos)
+        {
+            this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(milisegundos);
+        }
+
+        public void EsperarSegundos(int segundos)
+        {
+            this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(segundos);
+        }
+    }
+}
---
+++ test/functional_tests/Pages/Busqueda/Busqueda.cs
@@ -0,0 +1,56 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using LoCoMProTestFuncionales.PageModels.Busqueda;
+using LoCoMProTestFuncionales.PageModels.Home;
+
+namespace LoCoMProTestFuncionales.Pages.Busqueda
+{
+    [TestFixture]
+    public class BusquedaTests
+    {
+        private IWebDriver driver;
+        private PaginaBusqueda paginaBusqueda;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Preparación
+            driver = new ChromeDriver();
+        }
+
+        // Alumno: Enrique Guillermo Vílchez Lizano C18477 - Sprint 2
+        [Test]
+        public void PaginaBusqueda_FiltradoPorProvinciaGuanacaste_FilasDebenTenerSoloProvinciaGuanacaste()
+        {
+            // Preparación
+            PaginaHome paginaHome = new PaginaHome(this.driver);
+            driver.Navigate().GoToUrl(paginaHome.ObtenerURL());
+
+            this.paginaBusqueda = paginaHome.Buscar("i");
+
+            // Acción
+            this.paginaBusqueda.FiltrarConCajasDeSeleccion("provincia", "Guanacaste");
+            List<List<string>> resultados = this.paginaBusqueda.ObtenerTablaDeResultados();
+
+            // Verificación
+            bool filtradoCorrecto = true;
+            foreach(var  resultado in resultados)
+            {
+                if (resultado[7] != "Guanacaste")
+                {
+                    filtradoCorrecto = false;
+                }
+            }
+            
+            Assert.IsTrue(filtradoCorrecto);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
+
