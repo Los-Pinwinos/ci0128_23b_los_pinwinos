951653dfacdc716e6ac4dc19a72bab663ba3ddf0
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2023-11-18 15:15:20 -0600

    Ventana de cambio de contraseña se ve similar al mockup.

---
+++ source/LoCoMPro/Pages/Cuenta/Contrasena.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages.Cuenta
+{
+    public class ModeloContrasena : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
