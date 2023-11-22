739a7b9e9ecaebff38fa11821571ec0df53a0325
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2023-11-22 11:37:37 -0600

    Diseño inicial para ventana de usuarios que más reportan. Se colocan los títulos de las filas y el título de la tabla con su respectivo css. Además, se coloca en la sección de layout correspondiente.

---
+++ source/LoCoMPro/Pages/Moderacion/EstadisticasUsuariosReportes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages.Moderacion
+{
+    public class EstadisticasUsuariosReportesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
