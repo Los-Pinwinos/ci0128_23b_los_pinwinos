0cdf93fd20c9479a742b805b7cf82d7c72ab9689
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2023-11-23 16:14:30 -0600

    Se implementa query inicial para obtener los usuarios que más reportes realizan. Además, se creó un ViewModel para manejar los datos de forma más cómoda. Todavía no se implementa que estos resultados se muestren de forma tabular.

--- source/LoCoMPro/Pages/Moderacion/EstadisticasUsuariosReportes.cshtml.cs
+++ source/LoCoMPro/Pages/Moderacion/EstadisticasUsuariosReportes.cshtml.cs
@@ -1,12 +1,62 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LoCoMPro.Data;
+using LoCoMPro.ViewModels.Moderacion;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks; // Import the necessary namespace for Task
 
 namespace LoCoMPro.Pages.Moderacion
 {
     public class EstadisticasUsuariosReportesModel : PageModel
     {
-        public void OnGet()
+        private readonly LoCoMProContext contexto;
+
+        public string resultadosEstadisticas { get; set; }
+
+        public IList<UsuarioEstadisticasVM> Usuarios { get; set; } = new List<UsuarioEstadisticasVM>();
+
+        public EstadisticasUsuariosReportesModel(LoCoMProContext context)
         {
+            contexto = context;
+        }
+
+        public IQueryable<UsuarioEstadisticasVM> buscarUsuariosReportadores()
+        {
+            var topUsers = contexto.Reportes
+                .Where(r => r.usuarioCreadorReporte != null)
+                .GroupBy(r => r.usuarioCreadorReporte)
+                .Select(g => new
+                {
+                    UsuarioCreadorReporte = g.Key,
+                    CountReports = g.Count(),
+                    CountVerifiedReports = g.Count(r => r.verificado),
+                })
+                .OrderByDescending(x => x.CountReports)
+                .Take(10)
+                .Join(contexto.Usuarios,
+                    report => report.UsuarioCreadorReporte,
+                    usuario => usuario.nombreDeUsuario,
+                    (report, usuario) => new UsuarioEstadisticasVM
+                    {
+                        NombreUsuario = usuario.nombreDeUsuario,
+                        Calificacion = usuario.calificacion,
+                        CantidadReportes = report.CountReports,
+                        CantidadVerificados = report.CountVerifiedReports,
+                    });
+
+            return topUsers;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var topUsers = buscarUsuariosReportadores();
+            this.Usuarios = await topUsers.ToListAsync();
+            this.resultadosEstadisticas = JsonConvert.SerializeObject(Usuarios);
+
+            return Page();
         }
     }
 }
---
+++ source/LoCoMPro/ViewModels/Moderacion/UsuariosEstadisticas.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoCoMPro.ViewModels.Moderacion
+{
+    public class UsuarioEstadisticasVM
+    {
+        public string NombreUsuario { get; set; }
+        public double Calificacion { get; set; }
+        public int CantidadReportes { get; set; }
+        public int CantidadVerificados { get; set; }
+
+    }
+}
