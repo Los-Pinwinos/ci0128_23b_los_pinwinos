88afcdaab9b8ee1f4e514f1c86b521b72f00e5aa
ENRIQUE GUILLERMO VILCHEZ LIZANO <enrique.vilchezlizano@ucr.ac.cr>
2023-11-23 08:23:46 -0600

    Se agregó un view model para la vista de favoritos.

---
+++ source/LoCoMPro/ViewModels/Cuenta/FavoritoVM.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoCoMPro.ViewModels.Cuenta
+{
+    public class FavoritoVM
+    {
+        // Nombre
+        [StringLength(256, MinimumLength = 1)]
+        [Display(Name = "Nombre del producto")]
+        public required string nombreProducto { get; set; }
+
+        // Nombre de la categoría
+        [StringLength(256, MinimumLength = 1)]
+        [RegularExpression(@"[a-zA-ZÀ-ÿ]+( ?[a-zA-ZÀ-ÿ])*")]
+        [Display(Name = "Categoría")]
+        public required string nombreCategoria { get; set; }
+
+        // Marca
+        [StringLength(256, MinimumLength = 1)]
+        [Display(Name = "Marca del producto")]
+        public string? nombreMarca { get; set; }
+    }
+}
