9e10330d05c8eeeb4132adde8e06111164fe6246
Angie Solis <angie.solismanzano@ucr.ac.cr>
2023-11-26 17:37:39 -0600

    Creación de página para OutliersPrecio

---
+++ source/LoCoMPro/Pages/Moderacion/OutliersPrecio.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages.Moderacion
+{
+    public class OutliersPrecioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
