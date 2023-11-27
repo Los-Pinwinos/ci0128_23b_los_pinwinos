81f702fd0a878fa61f1fd52bd989daabe1ce2ac3
Angie Solis <angie.solismanzano@ucr.ac.cr>
2023-11-26 18:39:46 -0600

    View model para los registros que se consultan para revisar los outliers de precio

---
+++ source/LoCoMPro/ViewModels/Moderacion/RegistroOutlierPrecioVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoCoMPro.ViewModels.Moderacion
+{
+    public class RegistroOutlierPrecioVM
+    {
+        [Display(Name = "Fecha")]
+        public required DateTime fecha { get; set; }
+
+        [Display(Name = "Usuario")]
+        public required string usuario { get; set; }
+
+        [Display(Name = "Producto")]
+        public required string producto { get; set; }
+
+        [Display(Name = "Precio")]
+        public required decimal precio { get; set; }
+
+        [Display(Name = "Tienda")]
+        public required string tienda { get; set; }
+
+        [Display(Name = "Provincia")]
+        public required string provincia { get; set; }
+
+        [Display(Name = "Cantón")]
+        public required string canton { get; set; }
+    }
+}
