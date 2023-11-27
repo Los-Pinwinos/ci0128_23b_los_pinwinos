e38abb432e78536699b23e44ae47310dfc66c847
Luis <luisdi2509@gmail.com>
2023-11-27 08:36:58 -0600

    Botón para ejecutar algoritmo en popup de moderación.

---
+++ source/LoCoMPro/Pages/Moderacion/ProductosSimilares.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages.Moderacion
+{
+    public class ProductosSimilares : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
