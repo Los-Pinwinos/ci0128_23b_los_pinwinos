1565eb243edd5351ea0c9de9852e6528f56d234a
Luis <luisdi2509@gmail.com>
2023-11-27 09:15:59 -0600

    Clase comparador utilizando el algoritmo Jaro-Winkler

---
+++ source/LoCoMPro/Utils/Clustering/Comparador.cs
@@ -0,0 +1,26 @@
+﻿using F23.StringSimilarity;
+
+namespace LoCoMPro.Utils.Clustering
+{
+    public class Comparador
+    {
+        private JaroWinkler algoritmo;
+
+        public Comparador()
+        {
+            this.algoritmo = new JaroWinkler();
+        }
+
+        public double comparacion(string primeraCadena, string segundaCadena)
+        {
+            if (this.algoritmo == null || primeraCadena == null || segundaCadena == null)
+            {
+                return -1;
+            }
+
+            double distancia = algoritmo.Similarity(primeraCadena, segundaCadena);
+
+            return distancia;
+        }
+    }
+}
