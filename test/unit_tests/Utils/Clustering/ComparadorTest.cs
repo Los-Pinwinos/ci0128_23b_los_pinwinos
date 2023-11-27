bfa5acd20008653026b739c0a744390851f5f3f1
Luis <luisdi2509@gmail.com>
2023-11-27 09:45:46 -0600

    Prueba de comparación de cadenas con primer valor nulo.

--- test/unit_tests/Utils/Clustering/ComparadorTest.cs
+++ test/unit_tests/Utils/Clustering/ComparadorTest.cs
@@ -46,6 +46,7 @@ namespace LoCoMProTests.Utils.Clustering
         [TestMethod]
         public void comparador_ValidacionComparacionPrimeroVacio_DeberiaDevolverMenosUno()
         {
+            // Preparación
             string primerParametro = "";
             string segundoParametro = "pruebaVacio";
             Comparador comparador = new Comparador();
@@ -63,8 +64,27 @@ namespace LoCoMProTests.Utils.Clustering
         [TestMethod]
         public void comparador_ValidacionComparacionSegundoVacio_DeberiaDevolverMenosUno()
         {
+            // Preparación
             string primerParametro = "pruebaVacio";
             string segundoParametro = "";
+            Comparador comparador = new Comparador();
+            // El rango de resultados es de [0,1], como se busca un -1 se establece 1
+            double resultado = 1;
+
+            // Acción
+            resultado = comparador.comparacion(primerParametro, segundoParametro);
+
+            // Verificación
+            Assert.AreEqual(-1.0, resultado);
+        }
+
+        // Hecho por: Luis David Solano Santamaría - C17634 - Sprint 3
+        [TestMethod]
+        public void comparador_ValidacionComparacionPrimeroNulo_DeberiaDevolverMenosUno()
+        {
+            // Preparación
+            string primerParametro = null;
+            string segundoParametro = "pruebaNulo";
             Comparador comparador = new Comparador();
             // El rango de resultados es de [0,1], como se busca un -1 se establece 1
             double resultado = 1;
