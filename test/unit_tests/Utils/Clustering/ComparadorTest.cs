8193f2307f64ead0b50c9b4d21100732c0a8256d
Luis <luisdi2509@gmail.com>
2023-11-27 09:26:09 -0600

    ComparadorTest creado y prueba de comparación con cadenas iguales

---
+++ test/unit_tests/Utils/Clustering/ComparadorTest.cs
@@ -0,0 +1,27 @@
+﻿using LoCoMPro.Utils.Clustering;
+
+namespace LoCoMProTests.Utils.Clustering
+{
+    // Hecho por: Luis David Solano Santamaría - C17634 - Sprint 3
+    [TestClass]
+    public class ComparadorTest
+    {
+        // Hecho por: Luis David Solano Santamaría - C17634 - Sprint 3
+        [TestMethod]
+        public void comparador_ValidacionComparacionIgual_DeberiaDevolverUno()
+        {
+            // Preparación
+            string primerParametro = "iphone 14";
+            string segundoParametro = "iphone 14";
+            Comparador comparador = new Comparador();
+            double resultado = -1.0;
+
+            // Acción
+            resultado = comparador.comparacion(primerParametro, segundoParametro);
+
+            // Verificación
+            Assert.AreEqual(1.0, resultado);
+        }
+
+    }
+}
