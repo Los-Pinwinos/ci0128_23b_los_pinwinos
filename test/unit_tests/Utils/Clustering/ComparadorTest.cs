a1ad9f0729bb6e599cf6c380f5d3b6a5412de2a3
Luis <luisdi2509@gmail.com>
2023-11-27 09:43:01 -0600

    Prueba de comparación con segunda cadena vacía.

--- test/unit_tests/Utils/Clustering/ComparadorTest.cs
+++ test/unit_tests/Utils/Clustering/ComparadorTest.cs
@@ -47,7 +47,24 @@ namespace LoCoMProTests.Utils.Clustering
         public void comparador_ValidacionComparacionPrimeroVacio_DeberiaDevolverMenosUno()
         {
             string primerParametro = "";
-            string segundoParametro = "pruebaNulo";
+            string segundoParametro = "pruebaVacio";
+            Comparador comparador = new Comparador();
+            // El rango de resultados es de [0,1], como se busca un -1 se establece 1
+            double resultado = 1;
+
+            // Acción
+            resultado = comparador.comparacion(primerParametro, segundoParametro);
+
+            // Verificación
+            Assert.AreEqual(-1.0, resultado);
+        }
+
+        // Hecho por: Luis David Solano Santamaría - C17634 - Sprint 3
+        [TestMethod]
+        public void comparador_ValidacionComparacionSegundoVacio_DeberiaDevolverMenosUno()
+        {
+            string primerParametro = "pruebaVacio";
+            string segundoParametro = "";
             Comparador comparador = new Comparador();
             // El rango de resultados es de [0,1], como se busca un -1 se establece 1
             double resultado = 1;
