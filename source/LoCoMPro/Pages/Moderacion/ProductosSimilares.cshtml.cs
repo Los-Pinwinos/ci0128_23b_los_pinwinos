bd4c9a6d609d8a1490cf372985a97be09e6212e4
Luis <luisdi2509@gmail.com>
2023-12-01 11:06:13 -0600

    Implementación de BuscadorDeAgrupaciones.cs

--- source/LoCoMPro/Pages/Moderacion/ProductosSimilares.cshtml.cs
+++ source/LoCoMPro/Pages/Moderacion/ProductosSimilares.cshtml.cs
@@ -1,12 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LoCoMPro.ViewModels.Moderacion;
+using LoCoMPro.Data;
 
 namespace LoCoMPro.Pages.Moderacion
 {
     public class ProductosSimilares : PageModel
     {
-        public void OnGet()
+        protected readonly LoCoMProContext contexto;
+        protected readonly IConfiguration configuracion;
+        public string? resultados;
+        public ProductosSimilaresVM productosSimilaresVM { get; set; }
+
+        // Constructor
+        public ProductosSimilares(LoCoMProContext contexto, IConfiguration configuracion)
         {
+            this.productosSimilaresVM = new ProductosSimilaresVM
+            {
+                nombreProducto = "",
+                nombreCategoria = "",
+                nombreMarca = "",
+                unidad = ""
+            };
+            this.contexto = contexto;
+            this.configuracion = configuracion;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                resultados = "1";
+            }
+            else
+            {
+                ViewData["MensajeError"] = "Por favor ingrese al sistema.";
+            }
+            return Page();
         }
     }
 }
