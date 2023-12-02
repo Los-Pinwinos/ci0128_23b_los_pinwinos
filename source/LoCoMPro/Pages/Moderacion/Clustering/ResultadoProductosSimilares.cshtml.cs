ad5fb905286a596ca4c741c2f3bf300606467712
GoninDS <luisdi2509@gmail.com>
2023-12-02 07:31:13 -0600

    Creación de página para el resultado de clustering y reacomodo de carpetas

---
+++ source/LoCoMPro/Pages/Moderacion/Clustering/ResultadoProductosSimilares.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages.Moderacion.Clustering
+{
+    public class ResultadoProductosSimilaresModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
